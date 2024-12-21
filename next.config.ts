242b32fa3f429cd7fb98396ceaedf72008739950
NeelParihar <neelparihar599@gmail.com>
2024-12-22 00:46:26 +0530

    Enhance styling and theming: update global CSS variables, integrate Inter font, and configure Tailwind for dark mode support
    Signed-off-by: NeelParihar <neelparihar599@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["source.unsplash.com", "img.freepik.com"],
+  },
 };
 
 export default nextConfig;
